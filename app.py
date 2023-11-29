9cb32407e57b4b4e3b0a253c4e58f9aeed89a89e
BrunoCarva1ho <brunowp01@gmail.com>
2023-11-29 09:10:37 -0300

    new commit

--- app.py
+++ app.py
@@ -13,11 +13,34 @@ for linha in sheet_produtos.iter_rows(min_row=2):
     pyautogui.hotkey('ctrl','v')
 
 
-    # descricao = linha[1].value
-    # categoria = linha[2].value
-    # codigo_produto = linha[3].value
-    # peso_kg = linha[4].value
-    # dimensoes = linha[5].value
+    descricao = linha[1].value
+    pyperclip.copy(descricao)
+    pyautogui.click(118,279, duration=1)
+    pyautogui.hotkey('ctrl','v')
+
+    categoria = linha[2].value
+    pyperclip.copy(categoria)
+    pyautogui.click(115,404, duration=1)
+    pyautogui.hotkey('ctrl','v')
+
+    codigo_produto = linha[3].value
+    pyperclip.copy(codigo_produto)
+    pyautogui.click(120,477, duration=1)
+    pyautogui.hotkey('ctrl','v')
+
+    peso_kg = linha[4].value
+    pyperclip.copy(peso_kg)
+    pyautogui.click(117,558, duration=1)
+    pyautogui.hotkey('ctrl','v')
+    
+
+    dimensoes = linha[5].value
+    pyperclip.copy(dimensoes)
+    pyautogui.click(115,636, duration=1)
+    pyautogui.hotkey('ctrl','v')
+    
+
+    
     # preco = linha[6].value
     # quantd_estoque = linha[7].value
     # data_validade = linha[8].value
